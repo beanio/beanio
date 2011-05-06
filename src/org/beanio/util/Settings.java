c7033b530e8efeaf65ec02972c97f05d1383cb1b
kevin.seim <kevin.seim@27034f29-5f44-6397-fbff-12959c28e2fc>
2011-05-06 20:38:32 +0000

    [1.1.0] Completed XML header logic.  Added support for namespace declarations on the root element in generated documents.

--- src/org/beanio/util/Settings.java
+++ src/org/beanio/util/Settings.java
@@ -48,7 +48,9 @@ public class Settings {
     /** The default date format pattern for fields assigned type alias <tt>Time</tt> */
     public static final String DEFAULT_TIME_FORMAT = "org.beanio.defaultTimeFormat";
     /** The default XML type for a field definition, set to <tt>element</tt> or <tt>attribute</tt>. */
-    public static final String DEFAULT_XML_TYPE = "org.beanio.defaultXmlType";
+    public static final String DEFAULT_XML_TYPE = "org.beanio.xml.defaultXmlType";
+    /** The default namespace prefix for 'http://www.w3.org/2001/XMLSchema-instance' */
+    public static final String DEFAULT_XSI_NAMESPACE_PREFIX = "org.beanio.xml.xsiNamespacePrefix";
     
     private static final String DEFAULT_CONFIGURATION_PATH = "/org/beanio/config/beanio.properties";
     private static final String DEFAULT_CONFIGURATION_FILENAME = "beanio.properties";

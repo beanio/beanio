0286650c4e2e6cfe249ca711b9021697be90fa8c
kevin.seim <kevin.seim@fa12aebc-18e6-4bd0-a00b-c41c8e91b89f>
2013-02-16 17:11:18 +0000

    [2.0.4] Fixed possible IOException when the last record in a file is malformed (gc0060)
    git-svn-id: svn://localhost/trunk@163 fa12aebc-18e6-4bd0-a00b-c41c8e91b89f

--- test/org/beanio/stream/CsvReaderTest.java
+++ test/org/beanio/stream/CsvReaderTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2010-2011 Kevin Seim
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.beanio.stream;
 
 import static org.junit.Assert.*;
@@ -327,6 +312,25 @@ public class CsvReaderTest {
         assertNull(in.read());
     }
 
+    @Test
+    public void testMalformedRecordAtEOF() throws IOException {
+        
+        StrictStringReader input = new StrictStringReader("\"hello,ma");
+        
+        RecordIOException error = null;
+        
+        CsvReader in = new CsvReader(input);
+        try {
+            in.read();
+        }
+        catch (RecordIOException ex) {
+            error = ex;
+        }
+        
+        Assert.assertNotNull(error);
+        Assert.assertNull(in.read());
+    }
+    
     @SuppressWarnings("unused")
     private void print(String[] sa) {
         for (String s : sa) {

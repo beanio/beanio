ebd7ba75b691f2dee56bf8ac6498e003da94172e
kevin.seim <kevin.seim@27034f29-5f44-6397-fbff-12959c28e2fc>
2013-05-21 02:29:47 +0000

    [2.0.6] Tagging the 2.0.6 release

---
+++ tags/2.0.6/src/org/beanio/internal/config/BeanConfig.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2010-2011 Kevin Seim
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.beanio.internal.config;
+
+import java.util.Properties;
+
+/**
+ * Stores bean information.  A <tt>Bean</tt> object is used to instantiate 
+ * configurable components such as a type handler, record reader factory
+ * or record writer factory.
+ * 
+ * @author Kevin Seim
+ * @since 1.0
+ */
+public class BeanConfig {
+
+    private String className;
+    private Properties properties;
+
+    /**
+     * Returns the fully qualified class name of the bean.
+     * @return the bean class name
+     */
+    public String getClassName() {
+        return className;
+    }
+
+    /**
+     * Sets the fully qualified class name of the bean.
+     * @param className the bean class name
+     */
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    /**
+     * Returns bean properties.
+     * @return the bean properties
+     */
+    public Properties getProperties() {
+        return properties;
+    }
+
+    /**
+     * Sets bean properties.
+     * @param properties the new bean properties
+     */
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+}
